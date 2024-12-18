4c7e5706c1d3b92d78948594bc2ea1396ecda2b0
Aleks C <useruser@192.168.1.102>
2024-12-18 14:08:05 +0300

    project created

---
+++ src/main/java/com/greenschedule/green_schedule/GreenScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.greenschedule.green_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenschedule/green_schedule/GreenScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenschedule.green_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
