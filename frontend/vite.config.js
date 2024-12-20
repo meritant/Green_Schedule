8e66fa4a79ecd4a7dcc195658cb8f58e8018ebf4
Aleks C <useruser@Users-MacBook-Pro.local>
2024-12-20 15:58:40 +0300

    Frontend Recover Point before main change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,8 +9,11 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
-        secure: false,
+        secure: false
       }
     }
+  },
+  optimizeDeps: {
+    exclude: ['fsevents']
   }
 })
